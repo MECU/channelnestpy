15510a7ce461e3d44d33952401a8f9b97b3d0417
Michael Hoppes <hoppes@gmail.com>
2017-05-26 10:51:47 -0600

    Fix check-video

--- channelnest/views.py
+++ channelnest/views.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-import re, requests, datetime, json
+import re, requests, datetime
 
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404, JsonResponse
-from .models import Video
+from django.http import JsonResponse
+from .models import Video, Type
 
 
 # output = ', '.join([v.title for v in latest_videos_list])
 def index(request):
-    videos = Video.objects.order_by('date_created')[:3]
+    videos = Video.objects.order_by('-date_created')[:3]
     context = {'videos': videos}
     return render(request, 'video/index.html', context)
 
@@ -23,29 +23,31 @@ def videoSubmit(request):
 
 
 def videoCheck(request):
-    videoURL = request.POST['url']
+    videoURL = request.GET.get('url')
 
-    matches = re.search('/https?:\/\/video.nest.com\/clip\/([a-f0-9]{32})(\.mp4)?/',videoURL)
+    matches = re.search('https?:\/\/video.nest.com\/clip\/([a-f0-9]{32})[\.mp4]+', videoURL)
     if not matches:
-        raise Http404("That doesn't appear to be a valid URL")
+        return JsonResponse({'error': "That doesn't appear to be a valid URL", 'matches': matches, 'url': videoURL})
 
     # See if we already have it
-    video = Video.objects.get(pk=matches.group(1))
+    video = Video.objects.filter(pk=matches.group(1)).first()
+
     if video:
-        raise HttpResponse("We already have this video.", status=400)
+        return JsonResponse({'error': "We already have this video."})
 
     # See if it exists at Nest
     response = requests.head(videoURL)
     if response.status_code == requests.codes.ok:
         # We got one!
-        video = Video(id=matches.group(1), title='TBD', type=1, date_created=datetime.now())
+        type = Type.objects.get(pk=1)
+        video = Video(id=matches.group(1), title='TBD', type=type, date_created=datetime.datetime.now())
 
         # Get the page proper to get the title
         page = requests.get(videoURL)
         if page.status_code == requests.codes.ok:
-            rawTitle = re.search("/\<title\>(.*)\<\/title\>/i", page.content)
+            rawTitle = re.search('\<title\>(.*)\<\/title\>', page.text)
             title = rawTitle.group(1)
-            title.replace(' | Nest', '')
+            title = title.replace(' | Nest', '')
             video.title = title
 
         video.save()
